3c53c569ff1e5c482d94f635b5e1029e63e09a85
KawanLopes-0 <kawanlopes123k@gmail.com>
2026-09-08 12:09:54 -0300

    Refatora operações bancárias com funções

--- main.py
+++ main.py
@@ -8,10 +8,28 @@ def exibir_menu():
     print(menu)
     return int(input("Escolha uma opção: "))
 
-selecao = exibir_menu()
+def depositar(valor, saldo, extrato):
 
-deposito = 0
-saque = 0
+    if valor <= 0:
+        print(f"Não é possivel depositar valor negativo ou igual a zero\n")
+    else:
+        saldo += valor
+        extrato.append(f"Depósito: R$ +{valor:.2f}")
+        print(f"Depósito de R$ {valor:.2f} realizado com sucesso!\n")
+    return saldo
+
+def sacar(saque, saldo, extrato):
+    if saque <= 0:
+        print(f"Não é possivel sacar valor negativo ou igual a zero\n")
+    elif saque > saldo:
+        print(f"Saldo insuficiente para realizar o saque de R$ {saque:.2f}\n")
+    else:
+        saldo -= saque
+        extrato.append(f"Saque: R$ -{saque:.2f}")
+        print(f"Saque de R$ {saque:.2f} realizado com sucesso!\n")
+    return saldo
+
+selecao = exibir_menu()
 saldo = 0
 extrato = []
 
@@ -19,27 +37,13 @@ while selecao != 5:
 
     if selecao == 1:
         print(f"===== DEPOSITAR =====\n")
-        deposito = float(input(f"Digite o valor do depósito:\n"))
-        
-        if deposito <= 0:
-            print(f"Não é possivel depositar valor negativo ou igual a zero\n")
-        else:
-            saldo += deposito
-            extrato.append(f"Depósito: R$ +{deposito:.2f}")
-            print(f"Depósito de R$ {deposito:.2f} realizado com sucesso!\n")
-        
+        valor = float(input(f"Digite o valor do depósito:\n"))
+        saldo = depositar(valor, saldo, extrato)
+
     elif selecao == 2:
         print(f"===== SACAR =====\n")
         saque = float(input(f"Digite o valor do saque:\n"))
-        
-        if saque <= 0:
-            print(f"Não é possivel sacar valor negativo ou igual a zero\n")
-        elif saque > saldo:
-            print(f"Saldo insuficiente para realizar o saque de R$ {saque:.2f}\n")
-        else:
-            saldo -= saque
-            extrato.append(f"Saque: R$ -{saque:.2f}")
-            print(f"Saque de R$ {saque:.2f} realizado com sucesso!\n")
+        saldo = sacar(saque, saldo, extrato)
         
     elif selecao == 3:
         print(f"===== CONSULTAR SALDO =====\n")
